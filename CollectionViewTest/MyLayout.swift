1edfa664b5fd0cdda4c864bba35150dee83249a6
Georgy <georgy@keyapps.co>
2023-07-29 21:47:47 +0400

    Fixed problem with the last item in a row

--- CollectionViewTest/MyLayout.swift
+++ CollectionViewTest/MyLayout.swift
@@ -7,63 +7,18 @@
 
 import UIKit
 
-//class AlignLeftFlowLayout: UICollectionViewFlowLayout {
-//
-//    var maximumCellSpacing = CGFloat(9.0)
-//
-//    override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]? {
-//        let attributesToReturn = super.layoutAttributesForElementsInRect(rect) as? [UICollectionViewLayoutAttributes]
-//
-//        for attributes in attributesToReturn ?? [] {
-//            if attributes.representedElementKind == nil {
-//                attributes.frame = self.layoutAttributesForItemAtIndexPath(attributes.indexPath).frame
-//            }
-//        }
-//
-//        return attributesToReturn
-//    }
-//
-
-
-
-//    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes! {
-//        let curAttributes = super.layoutAttributesForItemAtIndexPath(indexPath)
-//        let sectionInset = (self.collectionView?.collectionViewLayout as UICollectionViewFlowLayout).sectionInset
-//
-//        if indexPath.item == 0 {
-//            let f = curAttributes.frame
-//            curAttributes.frame = CGRectMake(sectionInset.left, f.origin.y, f.size.width, f.size.height)
-//            return curAttributes
-//        }
-//
-//        let prevIndexPath = NSIndexPath(forItem: indexPath.item-1, inSection: indexPath.section)
-//        let prevFrame = self.layoutAttributesForItemAtIndexPath(prevIndexPath).frame
-//        let prevFrameRightPoint = prevFrame.origin.x + prevFrame.size.width + maximumCellSpacing
-//
-//        let curFrame = curAttributes.frame
-//        let stretchedCurFrame = CGRectMake(0, curFrame.origin.y, self.collectionView!.frame.size.width, curFrame.size.height)
-//
-//        if CGRectIntersectsRect(prevFrame, stretchedCurFrame) {
-//            curAttributes.frame = CGRectMake(prevFrameRightPoint, curFrame.origin.y, curFrame.size.width, curFrame.size.height)
-//        } else {
-//            curAttributes.frame = CGRectMake(sectionInset.left, curFrame.origin.y, curFrame.size.width, curFrame.size.height)
-//        }
-//
-//        return curAttributes
-//    }
-//}
-
 open class MyLayout: UICollectionViewFlowLayout {
+    
+    private var cachedFramesOfItems: [IndexPath: CGRect] = [:]
+    
+    // Retrieves the layout attributes for ALL of the cells and views in the specified rectangle.
     open override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-//        return super.layoutAttributesForElements(in: rect)?.map { $0.representedElementKind == nil ? layoutAttributesForItem(at: $0.indexPath)! : $0 }
-        
-        let attributesToReturn = super.layoutAttributesForElements(in: rect) as? [UICollectionViewLayoutAttributes] ?? []
+        let attributesToReturn = super.layoutAttributesForElements(in: rect) ?? []
 
         for attributes in attributesToReturn {
             if attributes.representedElementKind == nil {
                 if let layoutForItem = self.layoutAttributesForItem(at: attributes.indexPath) {
                     attributes.frame = layoutForItem.frame
-                    print(layoutForItem.frame)
                 }
             }
         }
@@ -71,67 +26,41 @@ open class MyLayout: UICollectionViewFlowLayout {
         
     }
     
+    // Retrieves layout information for an item at the specified index path with a corresponding cell.
     open override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
         let curAttributes = super.layoutAttributesForItem(at: indexPath)
         
-        let maximumCellSpacing: CGFloat = 15
+        let maximumCellSpacing: CGFloat = 35
         
         if let curAttributes = curAttributes, let flowLayout = self.collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
-            
             let sectionInset = flowLayout.sectionInset
                 
             if indexPath.item == 0 {
                 let f = curAttributes.frame
-                curAttributes.frame = CGRectMake(sectionInset.left, f.origin.y, f.size.width, f.size.height)
-                return curAttributes
-            }
-            
-            let prevIndexPath = IndexPath(item: indexPath.item-1, section: indexPath.section)
-            if let lafiaip = self.layoutAttributesForItem(at: prevIndexPath) {
-                let prevFrame = lafiaip.frame
-                let prevFrameRightPoint = prevFrame.origin.x +
-                                          prevFrame.size.width + maximumCellSpacing
+                curAttributes.frame =  CGRectMake(sectionInset.left, f.origin.y, f.size.width, f.size.height)
+            } else {
+                let prevIndexPath = IndexPath(item: indexPath.item - 1, section: indexPath.section)
                 
-                let curFrame = curAttributes.frame
-                let stretchedCurFrame = CGRectMake(0, curFrame.origin.y, self.collectionView!.frame.size.width, curFrame.size.height)
-                
-                if CGRectIntersectsRect(prevFrame, stretchedCurFrame) {
-                    curAttributes.frame = CGRectMake(prevFrameRightPoint, curFrame.origin.y, curFrame.size.width, curFrame.size.height)
-                } else {
-                    curAttributes.frame = CGRectMake(sectionInset.left, curFrame.origin.y, curFrame.size.width, curFrame.size.height)
+                if let prevItemFrame = cachedFramesOfItems[prevIndexPath] {
+                    let curFrame = curAttributes.frame
+                    
+                    if (prevItemFrame.maxX + maximumCellSpacing + curFrame.size.width <= self.collectionView!.frame.size.width - sectionInset.right) {
+                        curAttributes.frame = CGRectMake(prevItemFrame.maxX + maximumCellSpacing,
+                                                         prevItemFrame.minY,
+                                                         curFrame.size.width,
+                                                         curFrame.size.height)
+                    } else {
+                        curAttributes.frame = CGRectMake(sectionInset.left,
+                                                         prevItemFrame.maxY + maximumCellSpacing,
+                                                         curFrame.size.width,
+                                                         curFrame.size.height)
+                    }
                 }
             }
+            cachedFramesOfItems[indexPath] = curAttributes.frame
         }
         
-                return curAttributes
+        return curAttributes
     }
-    
-
-//    open override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-//        guard let currentItemAttributes = super.layoutAttributesForItem(at: indexPath)?.copy() as? UICollectionViewLayoutAttributes,
-//            collectionView != nil else {
-//            // should never happen
-//            return nil
-//        }
-//
-//        // if the current frame, once stretched to the full row intersects the previous frame then they are on the same row
-//        if indexPath.item != 0,
-//            let previousFrame = layoutAttributesForItem(at: IndexPath(item: indexPath.item - 1, section: indexPath.section))?.frame,
-//            currentItemAttributes.frame.intersects(CGRect(x: -.infinity, y: previousFrame.origin.y, width: .infinity, height: previousFrame.size.height)) {
-//            // the next item on a line
-//            currentItemAttributes.frame.origin.x = previousFrame.origin.x + previousFrame.size.width + evaluatedMinimumInteritemSpacingForSection(at: indexPath.section)
-//        } else {
-//            // the first item on a line
-//            currentItemAttributes.frame.origin.x = evaluatedSectionInsetForSection(at: indexPath.section).left
-//        }
-//        return currentItemAttributes
-//    }
-//
-//    func evaluatedMinimumInteritemSpacingForSection(at section: NSInteger) -> CGFloat {
-//        return (collectionView?.delegate as? UICollectionViewDelegateFlowLayout)?.collectionView?(collectionView!, layout: self, minimumInteritemSpacingForSectionAt: section) ?? minimumInteritemSpacing
-//    }
-//
-//    func evaluatedSectionInsetForSection(at index: NSInteger) -> UIEdgeInsets {
-//        return (collectionView?.delegate as? UICollectionViewDelegateFlowLayout)?.collectionView?(collectionView!, layout: self, insetForSectionAt: index) ?? sectionInset
-//    }
 }
+
--- CollectionViewTest/ViewController.swift
+++ CollectionViewTest/ViewController.swift
@@ -5,18 +5,26 @@
 //  Created by Yura on 28.07.2023.
 //
 
+
+// info https://stackoverflow.com/questions/25895311/uicollectionview-self-sizing-cells-with-auto-layout
+
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var colView: UICollectionView!
     
-    var arr = ["fnef", "qdknwk",  "feknwknw", "fnfn", "ee", "ghruhrurhgu", "qbq", "wqbqqbqnwbnw", "nenje" ]
+    var arr = ["fnef", "qdknwk",  "feknwknw", "fnfn", "ee", "ghruhrurhgu",  "qbq", "wqbqqbqnwbnw", "qbq", "nenje" ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         colView.dataSource = self
         colView.delegate = self
+        
+        if let flowLayout = colView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+        
     }
 }
 
@@ -28,6 +36,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as! MyCell
+        
         cell.lbl.text = arr[indexPath.row]
         return cell
     }
@@ -35,13 +44,14 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let text = arr[indexPath.row]
-        let size = text.size(
-            withAttributes:[.font: UIFont.systemFont(ofSize: 17.0)])
-        
-        let  randomWidth = size.width + 30
-        return CGSize(width: CGFloat(randomWidth ), height: 60);
-    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        let text = arr[indexPath.row]
+//        let font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body) //UIFont.systemFont(ofSize: 17.0)
+//        let size = text.size(
+//            withAttributes:[.font: font])
+//
+//        let  width = size.width + 28
+//        return CGSize(width: CGFloat(width), height: 60);
+//    }
 }
